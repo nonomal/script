1748338dac6a170808d23fde5a5866b7176ef58f
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-08-20 14:53:54 +0800

    Update wsgj.js

--- JB/wsgj.js
+++ JB/wsgj.js
@@ -13,61 +13,62 @@ hostname = api-v3.speedtest.cn
 
 
 
-var chxm = JSON.parse($response.body); 
+ var chxm = JSON.parse($response.body); 
 // 
 
 chxm ={
-  "code": 0,
-  "data": {
-    "id": "11641894",
-    "username": "chxm1023",
-    "email": null,
-    "phone": "15305051023",
-    "nickname": "chxm1023",
-    "avatar": "https://file2.speedtest.cn/avatar/011/68/49/34/CUFrcGqPso5lGSiSfIX7Whq16x4ejde2X5CvoVYd.jpeg",
-    "gender": null,
-    "identified": "0",
-    "disabled": "0",
-    "password": "",
-    "balance": 0,
-    "integral": "123456789",
-    "status": "1",
-    "udid": null,
-    "phoneProvince": null,
-    "phoneCity": null,
-    "vpnRegister": "0",
-    "frozenIntegral": "0",
-    "inviteId": "0",
-    "winxinOpenid": null,
-    "rememberToken": null,
-    "loginCheck": null,
-    "emailVerifiedAt": null,
-    "isMajor": "1",
-    "receiveSms": "0",
-    "createdAt": "2022-06-22 20:53:20",
-    "updatedAt": "2022-06-23 02:16:56",
-    "deletedAt": null,
-    "inviteTime": null,
-    "vipExpire": "2099-09-09 21:21:21",
-    "hashId": "veqPdo9J0WxOBwQzNj60",
-    "profile": {
-      "birthday": null,
-      "location": null,
-      "address": null,
-      "website": null,
-      "timezone": null,
-      "introduction": null,
-      "bio": null,
-      "userId": "11641894",
-      "countryCode": null,
-      "provinceId": null,
-      "cityId": null
+  "msg" : "ok",
+  "data" : {
+    "updatedAt" : "2022-08-09 15:01:49",
+    "id" : "11684934",
+    "integral" : "666666666",
+    "phoneCity" : null,
+    "vpnRegister" : "0",
+    "loginCheck" : null,
+    "phoneProvince" : null,
+    "hashId" : "WPNv2bRO61bqQ9Vedz5Y",
+    "profile" : {
+      "location" : null,
+      "address" : null,
+      "website" : null,
+      "introduction" : null,
+      "bio" : null,
+      "userId" : "11684934",
+      "birthday" : null,
+      "timezone" : null,
+      "countryCode" : null,
+      "provinceId" : null,
+      "cityId" : null
     },
-    "isVip": 1,
-    "socialId": "oGACExIzJUwafX_X6TCuRjwQNpuw",
-    "socialNickname": "chxm1023"
+    "nickname" : "聪",
+    "disabled" : "0",
+    "emailVerifiedAt" : null,
+    "rememberToken" : null,
+    "email" : null,
+    "iPhone" : "13900001023"
+    "identified" : "0",
+    "receiveSms" : "0",
+    "createdAt" : "2022-07-30 01:44:25",
+    "vipExpire" : "2099-09-09 09:09:09",
+    "username" : "聪1023",
+    "avatar" : "https://file2.speedtest.cn/avatar/011/68/49/34/CUFrcGqPso5lGSiSfIX7Whq16x4ejde2X5CvoVYd.jpeg",
+    "status" : "1",
+    "isMajor" : "1",
+    "inviteId" : "0",
+    "isVip" : 1,
+    "winxinOpenid" : null,
+    "gender" : null,
+    "frozenIntegral" : "0",
+    "socialNickname" : "聪",
+    "deletedAt" : null,
+    "balance" : 0,
+    "socialId" : "oGACExPylvydWhm2SJHAUfHV7geE",
+    "password" : "",
+    "phone" : null,
+    "inviteTime" : null,
+    "udid" : null
   },
-  "msg": "ok"
+  "code" : 0
 }
 
 $done({body:JSON.stringify(chxm)});
