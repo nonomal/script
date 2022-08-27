9a1ffb5c2bce1b116996ebce1e8a74a62948e6f1
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-08-27 20:31:01 +0800

    Update xcxad.js

--- JB/xcxad.js
+++ JB/xcxad.js
@@ -7,8 +7,7 @@ hostname = oto.yunw2.cn, q12.tuanyougou.com, www.bbfx.ltd,qadx.qinlinad.com
 https://qadx.qinlinad.com url reject
 
 # 科技窝，www.bbfx.ltd
-https://www.bbfx.ltd/wp-json/watch-life-net/v1/options url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
-https://www.bbfx.ltd/wp-json/watch-life-net/v1/posts url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
+https://www.bbfx.ltd/wp-json/watch-life-net/v1 url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
 
 # 海绵去水印，q12.tuanyougou.com
 https://q12.tuanyougou.com/advert/* url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
