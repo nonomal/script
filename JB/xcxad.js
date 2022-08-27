7a6394545d8d588f195a853e831b2182d337bbe3
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-08-27 20:26:43 +0800

    Update xcxad.js

--- JB/xcxad.js
+++ JB/xcxad.js
@@ -1,17 +1,14 @@
-
-
-
-
-
+[mitm]
 hostname = oto.yunw2.cn, q12.tuanyougou.com, www.bbfx.ltd,qadx.qinlinad.com
 
+
+[rewrite_local]
 # 亲邻开门+去广告
 https://qadx.qinlinad.com url reject
 
-
-#  科技窝，www.bbfx.ltd
+# 科技窝，www.bbfx.ltd
 https://www.bbfx.ltd/wp-json/watch-life-net/v1/options url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
-https://www.bbfx.ltd/wp-json/watch-life-net/v1/posts  url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
+https://www.bbfx.ltd/wp-json/watch-life-net/v1/posts url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
 
 # 海绵去水印，q12.tuanyougou.com
 https://q12.tuanyougou.com/advert/* url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
