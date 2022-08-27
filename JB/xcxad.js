c779e8ed61a9f28ad3118ccf6aad74ae2a3f9151
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-08-27 20:01:03 +0800

    Create xcxad.js

---
+++ JB/xcxad.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+hostname = oto.yunw2.cn, q12.tuanyougou.com, www.bbfx.ltd,qadx.qinlinad.com
+
+# 亲邻开门+去广告
+https://qadx.qinlinad.com url reject
+
+
+#  科技窝，www.bbfx.ltd
+https://www.bbfx.ltd/wp-json/watch-life-net/v1/options url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
+https://www.bbfx.ltd/wp-json/watch-life-net/v1/posts  url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
+
+# 海绵去水印，q12.tuanyougou.com
+https://q12.tuanyougou.com/advert/* url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
+
+# 步走赚，oto.yunw2.cn
+https://oto.yunw2.cn/app/index.php* url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/JB/xcx.js
+
