09d659766b9af8799bc9b673b791a4b6a0e5261f
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-09-16 16:05:40 +0800

    Update tieba_cookie.js

--- JB/tieba_cookie.js
+++ JB/tieba_cookie.js
@@ -3,3 +3,6 @@
 ^https?:\/\/(c\.tieba\.baidu\.com|180\.97\.\d+\.\d+)\/c\/s\/login url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/JB/tieba_signin.js
 ^https?:\/\/c\.tieba\.baidu\.com\/c\/s\/channelIconConfig url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/JB/tieba_signin.js
 ^https?:\/\/tiebac\.baidu\.com\/c\/u\/follow\/getFoldedMessageUserInfo url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/JB/tieba_signin.js
+
+
+hostname= c.tieba.baidu.com
