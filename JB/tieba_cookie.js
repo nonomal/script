7bbcccaa198ab35588901e6ea0a3df21ddf174ed
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-09-16 15:58:24 +0800

    Create tieba_cookie.js

--- JB/tieba_cookie.js
+++ JB/tieba_cookie.js
@@ -1,7 +1,5 @@
 # 百度贴吧每日自动签到
 
-^https?:\/\/(c\.tieba\.baidu\.com|180\.97\.\d+\.\d+)\/c\/s\/login url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/JB/tieba_cookie.js
-^https?:\/\/c\.tieba\.baidu\.com\/c\/s\/channelIconConfig url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/JB/tieba_cookie.js
-^https?:\/\/tiebac\.baidu\.com\/c\/u\/follow\/getFoldedMessageUserInfo url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/JB/tieba_cookie.js
-
-hostname = c.tieba.baidu.com
+^https?:\/\/(c\.tieba\.baidu\.com|180\.97\.\d+\.\d+)\/c\/s\/login url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/JB/tieba_signin.js
+^https?:\/\/c\.tieba\.baidu\.com\/c\/s\/channelIconConfig url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/JB/tieba_signin.js
+^https?:\/\/tiebac\.baidu\.com\/c\/u\/follow\/getFoldedMessageUserInfo url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/JB/tieba_signin.js
