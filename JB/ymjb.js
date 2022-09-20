6fcd4f32ff3c887f16e44f5a7c1cf581452e9b0d
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-09-20 16:06:40 +0800

    Update ymjb.js

--- JB/ymjb.js
+++ JB/ymjb.js
@@ -15,7 +15,7 @@ hostname= apph5.manmanbuy.com, h5.ele.me, open.meituan.com, game.xiaojukeji.com,
 
 
 # 健步有礼获取Ck hostname= bwa.feierlaiedu.com
-https://bwa.feierlaiedu.com/api/v1/bbg/taskRecord/queryUserInfo url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/jzyl.js
+https://bwa.feierlaiedu.com/api/v1/bbg/taskRecord/queryUserInfo url script-request-header https://raw.githubusercontents.com/leafTheFish/DeathNote/main/jzyl.js
 
 
 
@@ -30,39 +30,39 @@ https://www.chongpar.cn/member/cp-member-integral/add url script-request-header 
 
 
 # 赶集直招 hostname= gjgrowthoper.58.com
-https://gjgrowthoper.58.com/user/userinfo url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/gjzz.js
+https://gjgrowthoper.58.com/user/userinfo url script-request-header https://raw.githubusercontents.com/leafTheFish/DeathNote/main/gjzz.js
 
 
 
 # 快手 hostname= api.kuaisho*.com
-appsupport/yoda/biz/info url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/ks.js
-ksapp/client/package/renew url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/ks.js
+appsupport/yoda/biz/info url script-request-header https://raw.githubusercontents.com/leafTheFish/DeathNote/main/ks.js
+ksapp/client/package/renew url script-request-header https://raw.githubusercontents.com/leafTheFish/DeathNote/main/ks.js
 
 
 
 # 快手极速版 hostname= api.kuaisho*.com
-appsupport/yoda/biz/info url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/ksjsb_v2.js
-ksapp/client/package/renew url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/ksjsb_v2.js
+appsupport/yoda/biz/info url script-request-header https://raw.githubusercontents.com/leafTheFish/DeathNote/main/ksjsb_v2.js
+ksapp/client/package/renew url script-request-header https://raw.githubusercontents.com/leafTheFish/DeathNote/main/ksjsb_v2.js
 
 
 
 # 滴滴果园 hostname= game.xiaojukeji.com
-https://game.xiaojukeji.com/api/game/plant/enter url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/ddgy.js
+https://game.xiaojukeji.com/api/game/plant/enter url script-request-header https://raw.githubusercontents.com/leafTheFish/DeathNote/main/ddgy.js
 
 
 
 # 美团 hostname= open.meituan.com
-https://open.meituan.com/user/v1/info url script-request-body https://raw.githubusercontent.com/leafTheFish/DeathNote/main/meituan.js
+https://open.meituan.com/user/v1/info url script-request-body https://raw.githubusercontents.com/leafTheFish/DeathNote/main/meituan.js
 
 
 
 # 饿了么-暂时失效 hostname= h5.ele.me
-; https://h5.ele.me/restapi/biz.growth_finetune/v1/finetune/operate? url ？script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/elm.js
+; https://h5.ele.me/restapi/biz.growth_finetune/v1/finetune/operate? url ？script-request-header https://raw.githubusercontents.com/leafTheFish/DeathNote/main/elm.js
 
 
 
 # 慢慢买获取Cookie hostname= apph5.manmanbuy.com
-^https?:\/\/apph5\.manmanbuy\.com\/taolijin\/(login.aspx|logserver.aspx) url script-request-body https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/manmanbuy/manmanbuy_checkin.js
+^https?:\/\/apph5\.manmanbuy\.com\/taolijin\/(login.aspx|logserver.aspx) url script-request-body https://raw.githubusercontents.com/blackmatrix7/ios_rule_script/master/script/manmanbuy/manmanbuy_checkin.js
 
 
 
