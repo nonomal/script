a76f1f1dcd1db19b80ca8dd35f603f0c0a906d3b
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-09-20 10:47:25 +0800

    Update cubox.js

--- JB/cubox.js
+++ JB/cubox.js
@@ -1,17 +1,12 @@
 /******************************
-脚本功能：cubox-收藏阅读+解锁VIP
+cubox-收藏阅读+解锁VIP
+
 下载地址：http://mtw.so/5IpOxH
-软件版本：7.1.1
-脚本作者：彭于晏
-更新时间：2022-9-19
-问题反馈：QQ+89996462
-QQ会员群：779392027
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 [rewrite_local]
-^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/cubox.js
+^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontents.com/chxm1023/script/main/JB/cubox.js
+
 [mitm] 
 hostname = cubox.pro
 *******************************/
@@ -26,8 +21,8 @@ const vip = '/userInfo';
 if (url.indexOf(vip) != -1) {
     obj.data.level = 1;
     obj.data.expireTime = "2099-09-12T23:50:23+08:00";
-    obj.data.nickName = "彭于晏解锁";
-    obj.data.thirdNickName = "彭于晏解锁";
+    obj.data.nickName = "chxm1023";
+    obj.data.thirdNickName = "chxm1023";
     obj.data.isExpire = false;
     obj.data.active = true;
     obj.data.payTime = 1660006006;
