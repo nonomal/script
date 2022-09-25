b04dbb03370e20801171fc0db5b00279a176f17b
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-09-25 17:21:49 +0800

    Update ddgy.js

--- JB/task/ddgy.js
+++ JB/task/ddgy.js
@@ -21,9 +21,9 @@ export ddgyToken='uid&token'
 重写：
 [task_local]
 #滴滴果园
-28 0,8,12,18 * * * https://raw.githubusercontent.com/leafTheFish/DeathNote/main/ddgy.js, tag=滴滴果园, enabled=true
+28 0,8,12,18 * * ? https://raw.githubusercontents.com/chxm1023/script/main/JB/task/ddgy.js, tag=滴滴果园, enabled=true
 [rewrite_local]
-https://game.xiaojukeji.com/api/game/plant/enter url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/ddgy.js
+https://game.xiaojukeji.com/api/game/plant/enter url script-request-header https://raw.githubusercontents.com/chxm1023/script/main/JB/task/ddgy.js
 [MITM]
 hostname = game.xiaojukeji.com
 
