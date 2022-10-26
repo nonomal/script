2a25264d9bac53b1932a0b285c667c717858a5c3
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-26 17:22:10 +0800

    Delete tieba_cookie.js

--- JB/tieba_cookie.js
+++
@@ -1,8 +0,0 @@
-# 百度贴吧每日自动签到获取Cookie
-
-^https?:\/\/(c\.tieba\.baidu\.com|180\.97\.\d+\.\d+)\/c\/s\/login url script-request-header https://raw.githubusercontents.com/chxm1023/script/main/JB/tieba_signin.js
-^https?:\/\/c\.tieba\.baidu\.com\/c\/s\/channelIconConfig url script-request-header https://raw.githubusercontents.com/chxm1023/script/main/JB/tieba_signin.js
-^https?:\/\/tiebac\.baidu\.com\/c\/u\/follow\/getFoldedMessageUserInfo url script-request-header https://raw.githubusercontents.com/chxm1023/script/main/JB/tieba_signin.js
-
-
-hostname= c.tieba.baidu.com
