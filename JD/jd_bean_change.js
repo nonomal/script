275d40ae59ee2574d46d8f3aa907b919bf8ed823
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-26 17:27:49 +0800

    Update jd_bean_change.js

--- JD/jd_bean_change.js
+++ JD/jd_bean_change.js
@@ -11,7 +11,7 @@ crontab="2 9 * * *"
 ============QuantumultX==============
 [task_local]
 #京东资产变动通知
-2 9 * * * jd_bean_change.js, tag=京东资产变动通知, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+2 9 * * ? https://raw.githubusercontent.com/chxm1023/script/main/JD/jd_bean_change.js, tag=京东资产变动加强通知, img-url=https://raw.githubusercontent.com/chxm1023/script/main/JD/icons/jd.png, enabled=true
 ================Loon===============
 [Script]
 cron "2 9 * * *" script-path=jd_bean_change.js, tag=京东资产变动通知
