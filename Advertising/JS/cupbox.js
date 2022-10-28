3da40a969fac117f08e7879db0f898eb41476444
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-28 15:03:07 +0800

    Update cupbox.js

--- Advertising/JS/cupbox.js
+++ Advertising/JS/cupbox.js
@@ -1,3 +1,11 @@
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/cupbox.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="{div[class*="mobile-nav"]{display: none! important}
+div[class*="model"]{display: none! important}
+div[class*="hongbao"]{display: none! important}
+div[class*="jsx-ad"]{display: none! important}
+.adsbygoogle{display: none! important}
+.banner-box{display: none! important}
+a[href*="shopping"]{display: none! important}
+a[href*="donate"]{display: none! important}
+div[class*="notice"]{display: none! important}}" type="text/css">');
 $done({ body });
