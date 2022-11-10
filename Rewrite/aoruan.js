9780b9142b4551b895e6e07c12b62ea9aeaf1da4
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-10 13:35:54 +0800

    Update aoruan.js

--- Rewrite/aoruan.js
+++ Rewrite/aoruan.js
@@ -1,7 +1,37 @@
 /****************************************
 
-项目功能：傲软全家桶 解锁VIP
-下载地址：http://mtw.so/5twaH6
+项目功能：傲软全家桶
+
+项目名称：傲软抠图 解锁Vip
+下载地址：https://t.cn/A6xBOE5d
+
+项目名称：傲软扫描 解锁Vip
+下载地址：https://t.cn/A6o1jHWR
+
+项目名称：傲软PDF转换 解锁Vip
+下载地址：https://t.cn/A6o1j588
+
+项目名称：傲软PDF编辑 解锁Vip
+下载地址：https://t.cn/A6o1jCGU
+
+项目名称：傲软投屏 解锁Vip
+下载地址：https://t.cn/A65nw9gx
+
+项目名称：咖映 解锁Vip
+下载地址：https://t.cn/A6o1lsFL
+
+项目名称：轻闪PDF 解锁Vip
+下载地址：https://t.cn/A6o1iiI2
+
+项目名称：乃糖小组件 解锁Vip
+下载地址：https://t.cn/A6o1iMdP
+
+项目名称：佐糖 解锁Vip
+下载地址：https://t.cn/A6o1iVTI
+
+项目名称：佐糖照片修复 解锁Vip
+下载地址：https://t.cn/A6o1itzG
+
 使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *****************************************
