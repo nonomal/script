fc6a7655538921819314488ee90108355a2501e1
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-10 23:17:31 +0800

    Update bdsp.js

--- Rewrite/bdsp.js
+++ Rewrite/bdsp.js
@@ -1,7 +1,7 @@
 /****************************************
 
 项目功能：布丁锁屏 解锁VIP
-下载地址：http://mtw.so/6rFR6J
+下载地址：https://t.cn/A6o11VGR
 使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *****************************************
@@ -17,6 +17,5 @@ hostname = screen-lock.51wnl-cq.com
 ****************************************/
 
 
-var cuttlefish ={"warning":"仅供学习，禁止转载或售卖","tgchannel":"https://t.me/chxm1023","feedback":"chxm1023"}
 var chxm1023 = {"returncode":"SUCCESS","errormsg":"","data":{"id":"chxm1023","nickName":"chxm1023","sign":"chxm1023","vipStatus":1,"endTime":2099,"exchangeCode":null,"expireDate":null}};
 $done({body: JSON.stringify(chxm1023)});
