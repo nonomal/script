e0e06a764aac4833fe8359fd8806bf1d79963c04
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-10 15:15:42 +0800

    Create wfzy.js

---
+++ Rewrite/wfzy.js
@@ -0,0 +1,22 @@
+/****************************************
+
+项目功能：悟饭掌悦 解锁Vip
+下载地址: https://t.cn/A6o114Kq
+使用说明：非一次性解锁，每次解锁需打开脚本
+使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*****************************************
+
+[rewrite_local]
+
+http:\/\/iosv2\.cjapi\.wufan88\.com\/user\/.+ url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/Rewrite/wfzy.js
+
+
+[mitm] 
+
+hostname = iosv2.cjapi.wufan88.com
+
+
+****************************************/
+
+
