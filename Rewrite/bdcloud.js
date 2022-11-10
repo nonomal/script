9c9b610dced716223d21f83e540db47e4df1bc30
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-10 19:28:35 +0800

    Update bdcloud.js

--- Rewrite/bdcloud.js
+++ Rewrite/bdcloud.js
@@ -51,7 +51,7 @@ var chxm1023 = JSON.parse($response.body);
       "buy_time" : 0,
       "product_id" : "1",
       "auto_upgrade_to_svip" : 0,
-      "end_time" : 1672502399,
+      "end_time" : 4092600296,
       "cluster" : "vip",
       "detail_cluster" : "svip",
       "status" : 0
