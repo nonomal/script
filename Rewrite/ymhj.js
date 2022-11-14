088babde634e80a023452864a8a7edffc81f2ae7
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-14 10:42:02 +0800

    Create ymhj.js

---
+++ Rewrite/ymhj.js
@@ -0,0 +1,48 @@
+/****************************************
+
+脚本介绍：薅羊毛合集，@chxm1023整理
+
+****************************************/
+
+[mitm]
+
+hostname= apph5.manmanbuy.com, open.meituan.com, game.xiaojukeji.com,api.kuaisho*.com,gjgrowthoper.58.com, bwa.feierlaiedu.com, magicisland.58.com, *.ele.me
+
+
+
+[rewrite_local]
+
+# 饿了么
+mtop.alsc.eleme.miniapp.collection.homepagev1 url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/elm.js
+
+
+# 健步有礼获取Ck hostname= bwa.feierlaiedu.com
+https://bwa.feierlaiedu.com/api/v1/bbg/taskRecord/queryUserInfo url script-request-header https://raw.githubusercontent.com/chxm1023/QX/main/Task/jzyl.js
+
+
+# 58同城 hostname= magicisland.58.com
+https://magicisland.58.com/web/sign/getIndexSignInInfo url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/58tc.js
+
+
+# 赶集直招 hostname= gjgrowthoper.58.com
+https://gjgrowthoper.58.com/user/userinfo url script-request-header https://raw.githubusercontent.com/chxm1023/QX/main/Task/gjzz.js
+
+
+# 快手 hostname= api.kuaisho*.com
+appsupport/yoda/biz/info url script-request-header https://raw.githubusercontent.com/chxm1023/QX/main/Task/ks.js
+ksapp/client/package/renew url script-request-header https://raw.githubusercontent.com/chxm1023/QX/main/Task/ks.js
+
+
+# 快手极速版 hostname= api.kuaisho*.com
+appsupport/yoda/biz/info url script-request-header https://raw.githubusercontent.com/chxm1023/QX/main/Task/ksjsb_v2.js
+ksapp/client/package/renew url script-request-header https://raw.githubusercontent.com/chxm1023/QX/main/Task/ksjsb_v2.js
+
+
+# 美团 hostname= open.meituan.com
+https://open.meituan.com/user/v1/info url script-request-body https://raw.githubusercontent.com/chxm1023/QX/main/Task/meituan.js
+
+
+# 慢慢买获取Cookie hostname= apph5.manmanbuy.com
+^https?:\/\/apph5\.manmanbuy\.com\/taolijin\/(login.aspx|logserver.aspx) url script-request-body https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/manmanbuy/manmanbuy_checkin.js
+
+
