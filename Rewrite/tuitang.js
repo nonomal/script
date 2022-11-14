00e9b4494bf27f4d624b8f3435797ab7de973f28
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-14 19:22:59 +0800

    Update tuitang.js

--- Rewrite/tuitang.js
+++ Rewrite/tuitang.js
@@ -16,7 +16,7 @@ hostname = api.duitang.com
 
 ****************************************/
 
-body = $response.body.replace(/\"vip":\w+/g, '\"vip":true').replace(/\"is_life_artist":\w+/g, '\"is_life_artist":true').replace(/\"isnew":\w+/g, '\"isnew":true').replace(/\"short_video":\w+/g, '\"short_video":true').replace(/\"vip_end_at_mills":\d+/g, '\"vip_end_at_mills":4092600296000').replace(/\"vip_level":\d+/g, '\"vip_level":11').replace(/\"is_certify_user":\w+/g, '\"is_certify_user":true').replace(/\"be_follow_count":\d+/g, '\"be_follow_count":1000000').replace(/\"follow_count":\d+/g, '\"follow_count":1000000').replace(/\"score":\d+/g, '\"score":1000000')
+body = $response.body.replace(/\"vip":\w+/g, '\"vip":true').replace(/\"is_life_artist":\w+/g, '\"is_life_artist":true').replace(/\"isnew":\w+/g, '\"isnew":true').replace(/\"short_video":\w+/g, '\"short_video":true').replace(/\"vip_end_at_mills":\d+/g, '\"vip_end_at_mills":4092599350000').replace(/\"vip_level":\d+/g, '\"vip_level":11').replace(/\"is_certify_user":\w+/g, '\"is_certify_user":true').replace(/\"be_follow_count":\d+/g, '\"be_follow_count":1000000').replace(/\"follow_count":\d+/g, '\"follow_count":1000000').replace(/\"score":\d+/g, '\"score":1000000')
 
 
 $done({body});
