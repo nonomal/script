e5a3d4d05989b2561bdf7b2dfd7d9e631fd0d4d5
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-23 14:12:32 +0800

    Update jd_cookie.js

--- Task/JD/jd_cookie.js
+++ Task/JD/jd_cookie.js
@@ -1,7 +1,4 @@
-[rewrite_local]
-
-# 京东Cookie 登录https://bean.m.jd.com/bean/signIndex.action 登录后点击右下角“我的”获取Cookie
+# 京东Cookie 登录网址：https://bean.m.jd.com/bean/signIndex.action
 ^https:\/\/(api\.m|me-api)\.jd\.com\/(client\.action\?functionId=signBean|user_new\/info\/GetJDUserInfoUnion\?) url script-request-header https://raw.githubusercontent.com/chxm1023/script/main/Task/JD/JD_DailyBonus.js
 
-[mitm] 
 hostname = ms.jr.jd.com, me-api.jd.com, api.m.jd.com
